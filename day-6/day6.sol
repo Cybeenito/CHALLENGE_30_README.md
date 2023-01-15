ae1b59f0d21a45409275ac4f0c384f687cae83d9
abhilov23 <113055824+abhilov23@users.noreply.github.com>
2023-01-16 01:40:51 +0530

    Create day6.sol
    Finding average of three numbers

---
+++ day-6/day6.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract day6 {
+    function average(int256 a, int256 b, int256 c) public pure returns (int256) {
+        require(a > 0 && b > 0 && c > 0, "numbers must not be negative");
+        return (a + b + c) / 3;
+    }
+}
