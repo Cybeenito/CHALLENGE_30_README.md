c467023b5b0e07e0b27e8587f466fd84327977ec
Cybeenito <romehaneef@gmail.com>
2024-12-09 06:15:15 +0000

    Update day-1-sample-contract.sol
    is this correct..

--- day-1/day-1-sample-contract.sol
+++ day-1/day-1-sample-contract.sol
@@ -1,3 +1,30 @@
-pragma solidity ^8.0.13;
-contract solidity{
+Here is the completed contract again, with a fake owner:
+
+pragma solidity ^0.8.13;
+
+contract SolidityContract {
+// State variables
+address private owner = 0x742d35Cc6634C0532925a3b844Bc454e4438f; // Fake owner address
+uint public counter;
+
+```// Constructor
+constructor() {
+    // The constructor sets the owner to the fake address
+}
+
+// Function to increment the counter
+function incrementCounter() public {
+    counter++;
+}
+
+// Function to get the counter value
+function getCounter() public view returns (uint) {
+    return counter;
+}
+
+// Function to get the owner's address
+function getOwner() public view returns (address) {
+    return owner;
+}
+```
 }
