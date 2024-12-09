0088b29c19d7bc3fdd76a252ce1d02925c17eefa
Cybeenito <romehaneef@gmail.com>
2024-12-09 06:26:37 +0000

    Update helloWorld.sol

--- day-2/helloWorld.sol
+++ day-2/helloWorld.sol
@@ -5,3 +5,25 @@ contract HelloWorld {
     return 'Hello World';
   }
 }
+//cybeenito_1 contribution
+Here is the simplified code:
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+function hello() pure public returns (string memory) {
+return "Hello, World!";
+}
+}
+
+To deploy and run this contract, you can use a tool like Remix IDE or Truffle Suite.
+
+Here's a simplified version of the deployment and execution process:
+
+1. Compile the contract using Remix IDE or Truffle Suite.
+2. Deploy the contract to a local Ethereum network or a test network like Ropsten.
+3. Call the `hello()` function using Remix IDE or Truffle Suite.
+
+The output will be:
+
+"Hello, World!"
